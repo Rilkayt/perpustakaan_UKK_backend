484c78aa748064e67da4fef2fa70d2ed4ad1643f
Rilkayt <rilkayt@gmail.com>
2024-02-19 22:32:02 +0700

    register DONE

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+Let's start with Bissmillahirrahmanirrahim
+
 # perpustakaan_UKK_backend
+
 Ini merupakan project perpustakaan digital dari paket UKK , yang mana ini merupakan aplikasi web (backend)
--- src/Register/register.controller.js
+++ src/Register/register.controller.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const response = require("../../resTemp");
 const prisma = require("../../db");
+const nodemailer = require("nodemailer");
 
 const router = express.Router();
+
+let otpData = { otp: "", exp: null };
+
+if (otpData.otp != "") {
+  let nowTime = new Date().getTime();
+
+  console.log("waktu sekarang: ", nowTime);
+  console.log("waktu berakhir: ", endTime);
+  if (nowTime > otpData.exp) {
+    otp = "";
+    console.log("waktu sekarang: ", nowTime);
+    console.log("waktu berakhir: ", endTime);
+    return "otp sudah tidak berlaku";
+  }
+}
 
 router.post("/", async (req, res) => {
   const inputRegister = req.body;
@@ -10,8 +26,65 @@ router.post("/", async (req, res) => {
   if (inputRegister.tipe == "ADMIN") {
     let checkCode =
       await prisma.$queryRaw`SELECT * FROM kode_admin WHERE Kode = ${inputRegister.kode_admin}`;
-    console.log(checkCode);
-    if (checkCode.length > 0) {
+    // console.log(checkCode);
+    if (inputRegister.otp == otpData.otp && inputRegister.otp != "") {
+      if (checkCode.length > 0) {
+        let checkUsername = await prisma.user.count({
+          where: {
+            Username: inputRegister.username,
+          },
+        });
+
+        let checkEmail = await prisma.user.count({
+          where: {
+            Email: inputRegister.email,
+          },
+        });
+
+        let checkNotelp = await prisma.user.count({
+          where: {
+            NoTelp: inputRegister.notelp,
+          },
+        });
+
+        console.log(checkNotelp);
+        if (checkUsername == 0 && checkEmail == 0 && checkNotelp == 0) {
+          await prisma.user
+            .create({
+              data: {
+                Username: inputRegister.username,
+                Password: inputRegister.password,
+                NoTelp: inputRegister.notelp,
+                Email: inputRegister.email,
+                NamaLengkap: inputRegister.namaLengkap,
+                Alamat: inputRegister.alamat,
+                Sekolah: inputRegister.sekolah,
+                Tipe: inputRegister.tipe,
+              },
+            })
+            .then(() => {
+              response(200, inputRegister, res, "Register Berhasil !");
+            });
+        } else if (checkUsername > 0) {
+          throw response(400, {}, res, "username sudah tersedia");
+        } else if (checkNotelp > 0) {
+          throw response(400, {}, res, "No telepon sudah tersedia");
+        } else if (checkEmail > 0) {
+          throw response(400, {}, res, "Email sudah tersedia");
+        }
+      } else {
+        throw response(
+          400,
+          {},
+          res,
+          "kode admin tidak ditemukan atau tidak valid"
+        );
+      }
+    } else {
+      response(400, {}, res, "otp tidak valid");
+    }
+  } else {
+    if (inputRegister.otp == otpData.otp && inputRegister.otp != "") {
       let checkUsername = await prisma.user.count({
         where: {
           Username: inputRegister.username,
@@ -32,20 +105,22 @@ router.post("/", async (req, res) => {
 
       console.log(checkNotelp);
       if (checkUsername == 0 && checkEmail == 0 && checkNotelp == 0) {
-        await prisma.user.create({
-          data: {
-            Username: inputRegister.username,
-            Password: inputRegister.password,
-            NoTelp: inputRegister.notelp,
-            Email: inputRegister.email,
-            NamaLengkap: inputRegister.namaLengkap,
-            Alamat: inputRegister.alamat,
-            Sekolah: inputRegister.sekolah,
-            Tipe: inputRegister.tipe,
-          },
-        });
-
-        response(200, inputRegister, res, "Register Berhasil !");
+        await prisma.user
+          .create({
+            data: {
+              Username: inputRegister.username,
+              Password: inputRegister.password,
+              NoTelp: inputRegister.notelp,
+              Email: inputRegister.email,
+              NamaLengkap: inputRegister.namaLengkap,
+              Alamat: inputRegister.alamat,
+              Sekolah: inputRegister.sekolah,
+              Tipe: inputRegister.tipe,
+            },
+          })
+          .then(() => {
+            response(200, inputRegister, res, "Register Berhasil !");
+          });
       } else if (checkUsername > 0) {
         throw response(400, {}, res, "username sudah tersedia");
       } else if (checkNotelp > 0) {
@@ -54,55 +129,40 @@ router.post("/", async (req, res) => {
         throw response(400, {}, res, "Email sudah tersedia");
       }
     } else {
-      throw response(
-        400,
-        {},
-        res,
-        "kode admin tidak ditemukan atau tidak valid"
-      );
+      response(400, {}, res, "otp tidak valid");
     }
-  } else {
-    let checkUsername = await prisma.user.count({
-      where: {
-        Username: inputRegister.username,
-      },
+  }
+});
+
+router.post("/send-otp", async (req, res) => {
+  const { email } = req.body;
+  let otpNumber = await Math.floor(1000 + Math.random() * 9000);
+
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    // port : 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_ACCOUNT,
+      pass: process.env.PASSWORD_ACCOUNT,
+    },
+  });
+
+  const mailConfig = {
+    from: { name: "perpustakaan digital", address: "rilkayt@gmail.com" }, // sender address
+    to: [email], // list of receivers
+    subject: "OTP Perpustakaan Digital", // Subject line
+    text: `Your OTP ${otpNumber}`, // plain text body
+  };
+
+  try {
+    await transporter.sendMail(mailConfig).then(() => {
+      otpData = { otp: otpNumber, exp: new Date().getTime() + 5 * 60 * 1000 };
+      response(200, {}, res, "OTP berhasil terkirim ke email yang disediakan");
     });
-
-    let checkEmail = await prisma.user.count({
-      where: {
-        Email: inputRegister.email,
-      },
-    });
-
-    let checkNotelp = await prisma.user.count({
-      where: {
-        NoTelp: inputRegister.notelp,
-      },
-    });
-
-    console.log(checkNotelp);
-    if (checkUsername == 0 && checkEmail == 0 && checkNotelp == 0) {
-      await prisma.user.create({
-        data: {
-          Username: inputRegister.username,
-          Password: inputRegister.password,
-          NoTelp: inputRegister.notelp,
-          Email: inputRegister.email,
-          NamaLengkap: inputRegister.namaLengkap,
-          Alamat: inputRegister.alamat,
-          Sekolah: inputRegister.sekolah,
-          Tipe: inputRegister.tipe,
-        },
-      });
-
-      response(200, inputRegister, res, "Register Berhasil !");
-    } else if (checkUsername > 0) {
-      throw response(400, {}, res, "username sudah tersedia");
-    } else if (checkNotelp > 0) {
-      throw response(400, {}, res, "No telepon sudah tersedia");
-    } else if (checkEmail > 0) {
-      throw response(400, {}, res, "Email sudah tersedia");
-    }
+  } catch (error) {
+    response(400, {}, res, "terjadi kesalahan pada system");
   }
 });
 
